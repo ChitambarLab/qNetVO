9b81c7322f24674eb46e313bff948d9374e2b30c
Brian Doolittle <brian.d.doolittle@gmail.com>
2021-06-22 15:28:05 -0400

    noisy qubit optimization

--- QNetOptimizer/gradient_descent.py
+++ QNetOptimizer/gradient_descent.py
@@ -1,6 +1,6 @@
 import pennylane as qml
 
-def gradient_descent(cost, init_settings, num_steps=150, step_size=0.1, sample_width=25):
+def gradient_descent(cost, init_settings, num_steps=150, step_size=0.1, sample_width=25, verbose=True):
     opt = qml.GradientDescentOptimizer(stepsize=step_size)    
 
     settings = init_settings
@@ -18,12 +18,19 @@ def gradient_descent(cost, init_settings, num_steps=150, step_size=0.1, sample_w
             scores.append(score)
             samples.append(i)
 
-            print("iteration : ",i, ", score : ", score)
-            print("settings :\n", settings, "\n")
+            if verbose:
+                print("iteration : ",i, ", score : ", score)
+                print("settings :\n", settings, "\n")
     
-    final_score = -(cost(settings))
+    opt_score = -(cost(settings))
     
-    scores.append(final_score)
+    scores.append(opt_score)
     samples.append(num_steps-1)
     
-    return (final_score, settings, scores, samples, settings_history)
+    return {
+        "opt_score": opt_score,
+        "opt_settings": settings,
+        "scores": scores,
+        "samples": samples,
+        "settings_history": settings_history
+    }
