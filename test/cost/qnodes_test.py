dff26bd6f8880a0452181ed127fa139630cfa5c4
Brian Doolittle <brian.d.doolittle@gmail.com>
2021-08-23 21:52:20 -0500

    n-local observables and measurements

--- test/cost/qnodes_test.py
+++ test/cost/qnodes_test.py
@@ -6,6 +6,20 @@ from context import QNetOptimizer as QNopt
 
 
 class TestObservables:
+    def test_parity_observable(self):
+        qubit_obs = QNopt.parity_observable([0])
+        assert np.all(qubit_obs.matrix == np.array([[1, 0], [0, -1]]))
+        assert qubit_obs.wires == qml.wires.Wires([0])
+
+        qubit2_obs = QNopt.parity_observable([0, 1])
+        assert np.all(qubit2_obs.matrix == np.diag(np.kron([1, -1], [1, -1])))
+        assert qubit2_obs.wires == qml.wires.Wires([0, 1])
+
+        qubit3_obs = QNopt.parity_observable([0, 1, 2])
+        qubit3_match = np.diag(np.kron([1, -1], np.kron([1, -1], [1, -1])))
+        assert np.all(qubit3_obs.matrix == qubit3_match)
+        assert qubit3_obs.wires == qml.wires.Wires([0, 1, 2])
+
     def test_local_parity_observables(self):
 
         measure_nodes = [
@@ -43,6 +57,29 @@ class TestQNodes:
         assert np.all(qnode([[0, 0], [0, 0]], [[], []]) == [1, 1])
         assert np.all(qnode([[np.pi, 0], [0, 0]], [[], []]) == [-1, 1])
         assert np.all(qnode([[np.pi, np.pi], [np.pi, 0]], [[], []]) == [1, -1])
+
+    def test_global_parity_expval_qnode(self):
+
+        prep_nodes = [
+            QNopt.PrepareNode(
+                1, [0, 1, 2, 3], lambda settings, wires: qml.BasisState(settings, wires=wires), 4
+            )
+        ]
+        meas_nodes = [
+            QNopt.MeasureNode(1, 2, [0], lambda settings, wires: None, 0),
+            QNopt.MeasureNode(1, 2, [1], lambda settings, wires: None, 0),
+            QNopt.MeasureNode(1, 2, [2], lambda settings, wires: None, 0),
+            QNopt.MeasureNode(1, 2, [3], lambda settings, wires: None, 0),
+        ]
+
+        ansatz = QNopt.NetworkAnsatz(prep_nodes, meas_nodes)
+        qnode = QNopt.global_parity_expval_qnode(ansatz)
+
+        assert qnode([[0, 0, 0, 0]], [[], [], [], []]) == 1
+        assert qnode([[0, 1, 0, 0]], [[], [], [], []]) == -1
+        assert qnode([[0, 0, 1, 1]], [[], [], [], []]) == 1
+        assert qnode([[1, 0, 1, 1]], [[], [], [], []]) == -1
+        assert qnode([[1, 1, 1, 1]], [[], [], [], []]) == 1
 
     def test_joint_probs_qnode(self):
         prep_nodes = [
