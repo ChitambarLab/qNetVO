9e3cf375b41ffd9fd213069608c5c1e1d6d00fdd
Brian Doolittle <brian.d.doolittle@gmail.com>
2021-10-12 16:40:52 -0500

    mac capacity exploration

--- test/information_test.py
+++ test/information_test.py
@@ -104,6 +104,28 @@ class TestBisenderMACMutualInfo:
                 np.ones(2) / 2,
                 (1, 1, 1),
             ),
+            (
+                np.array(
+                    [
+                        [1, 1, 0, 0, 0, 1, 1, 1, 0],
+                        [0, 0, 1, 1, 1, 0, 0, 0, 1],
+                    ]
+                ),
+                np.ones(3) / 3,
+                np.ones(3) / 3,
+                (0.9182958340544896, 0.9182958340544896, 0.9910760598382216),
+            ),
+            (
+                np.array(
+                    [
+                        [1, 1, 0, 0, 0, 1, 1, 1, 0],
+                        [0, 0, 1, 1, 1, 0, 0, 0, 1],
+                    ]
+                ),
+                np.array([0.5,0.5,0]),
+                np.array([0,0.5,0.5]),
+                (1, 1, 1),
+            ),
         ],
     )
     def test_bisender_mac_mutual_info(self, mac_behavior, priors_x, priors_y, exp_rates_tuple):
