2eabf73c58e97cc6e520f945846185ebbb4a79b0
Brian Doolittle <brian.d.doolittle@gmail.com>
2021-12-10 18:54:16 -0600

    cost function and tests

---
+++ QNetOptimizer/cost/mutual_info.py
@@ -0,0 +1,85 @@
+import pennylane as qml
+from pennylane import math
+from pennylane import numpy as np
+from .qnodes import joint_probs_qnode
+from ..information import shannon_entropy
+from ..utilities import mixed_base_num
+
+
+def mutual_info_cost_fn(ansatz, priors, postmap=np.array([]), **qnode_kwargs):
+    """Constructs an ansatz-specific mutual information cost function.
+
+    The mutual information quantifies the information shared by two distributions
+    :math:`X` and :math:`Y`.
+    This entropic quantity is expressed as
+
+    .. math::
+
+            I(Y;X) = H(Y) + H(X) - H(XY)
+
+    where :math:`H(X) = -\\sum_{i}P(X_i)\\log_2(P(X_i))` is the Shannon entropy.
+
+    The mutual information can be used to quantify the amount of communication between
+    a sender and receiver.
+    In a quantum prepare and measure network, we evaluate the mutual information between
+    the collections of preparation and measurement nodes.
+
+    :param ansatz: The ansatz circuit on which the mutual information is evalutated.
+    :type ansatz: NetworkAnsatz
+
+    :param priors: A list of prior distributions for the inputs of each preparation node.
+    :type priors: list[np.array]
+
+    :param postmap: The post-processing matrix mapping the bitstring output from the
+                    quantum device into the measurement node outputs.
+    :type postmap: np.array
+
+    :param qnode_kwargs: Keyword arguments passed to the execute qnodes.
+    :type qnode_kwargs: dictionary
+
+    :returns: A cost function ``mutual_info_cost(scenario_settings)`` parameterized by
+              the ansatz-specific scenario settings.
+    :rtype: Function
+    """
+
+    num_prep_inputs = [prep_node.num_in for prep_node in ansatz.prepare_nodes]
+    num_prep_nodes = len(ansatz.prepare_nodes)
+    net_num_in = math.prod(num_prep_inputs)
+    node_input_ids = [mixed_base_num(i, num_prep_inputs) for i in range(net_num_in)]
+
+    net_num_out = math.prod([meas_node.num_out for meas_node in ansatz.measure_nodes])
+
+    if len(postmap) == 0:
+        postmap = np.eye(2 ** len(ansatz.measure_wires))
+
+    px_vec = 1
+    for i in range(len(priors)):
+        px_vec = np.kron(px_vec, priors[i])
+
+    Hx = shannon_entropy(px_vec)
+
+    probs_qnode = joint_probs_qnode(ansatz, **qnode_kwargs)
+
+    def cost_fn(scenario_settings):
+        Hxy = 0
+        py_vec = np.zeros(net_num_out)
+        for (i, input_id_set) in enumerate(node_input_ids):
+
+            settings = ansatz.qnode_settings(
+                scenario_settings,
+                input_id_set[0:num_prep_nodes],
+                [0] * len(ansatz.measure_nodes),  # the measure settings are fixed.
+            )
+
+            p_mac = postmap @ probs_qnode(settings)
+
+            Hxy += shannon_entropy(p_mac * px_vec[i])
+            py_vec += p_mac * px_vec[i]
+
+        Hy = shannon_entropy(py_vec)
+
+        mutual_info = Hx + Hy - Hxy
+
+        return -(mutual_info)
+
+    return cost_fn
---
+++ test/cost/mutual_info_test.py
@@ -0,0 +1,87 @@
+import pytest
+import pennylane as qml
+from pennylane import numpy as np
+
+from context import QNetOptimizer as QNopt
+
+
+class TestMutualInfoCostFn:
+    @pytest.mark.parametrize(
+        "scenario_settings,priors,postmap,match",
+        [
+            (
+                [[np.zeros((3, 1))], [np.zeros((3, 1))]],
+                [np.ones(3) / 3],
+                np.array([[1, 0], [0, 1], [0, 1]]),
+                0.0,
+            ),
+            (
+                [[np.array([[0], [np.pi], [0]])], [np.array([[0], [-np.pi], [0]])]],
+                [np.ones(3) / 3],
+                np.array([[1, 0], [0, 1], [0, 1]]),
+                -0.9182958,
+            ),
+            (
+                [[np.array([[0], [np.pi], [0]])], [np.array([[0], [-np.pi], [0]])]],
+                [np.array([0.5, 0.5, 0])],
+                np.array([[1, 0], [0, 1], [0, 1]]),
+                -1,
+            ),
+        ],
+    )
+    def test_mutual_info_cost_qubit_33(self, scenario_settings, priors, postmap, match):
+
+        ansatz = QNopt.NetworkAnsatz(
+            [QNopt.PrepareNode(3, [0], QNopt.local_RY, 1)],
+            [QNopt.MeasureNode(1, 3, [0], QNopt.local_RY, 1)],
+        )
+
+        mutual_info = QNopt.mutual_info_cost_fn(ansatz, priors, postmap=postmap)
+
+        assert np.isclose(mutual_info(scenario_settings), match)
+
+    def test_mutual_info_2_senders(self):
+
+        ansatz = QNopt.NetworkAnsatz(
+            [
+                QNopt.PrepareNode(2, [0], QNopt.local_RY, 1),
+                QNopt.PrepareNode(2, [1], QNopt.local_RY, 1),
+            ],
+            [QNopt.MeasureNode(1, 4, [0, 1], QNopt.local_RY, 2)],
+        )
+
+        scenario_settings = [
+            [np.array([[0], [np.pi]]), np.array([[0], [np.pi]])],
+            [np.array([[0, 0]])],
+        ]
+
+        priors = [np.ones(2) / 2, np.ones(2) / 2]
+
+        mutual_info = QNopt.mutual_info_cost_fn(ansatz, priors)
+
+        assert np.isclose(mutual_info(scenario_settings), -2)
+
+
+class TestMutualInfoOptimimzation:
+    @pytest.mark.parametrize(
+        "priors,match", [([np.ones(3) / 3], 0.9182), ([np.array([0.5, 0.5, 0])], 1)]
+    )
+    def test_mutual_info_opt_qubit_33(self, priors, match):
+
+        ansatz = QNopt.NetworkAnsatz(
+            [QNopt.PrepareNode(3, [0], QNopt.local_RY, 1)],
+            [QNopt.MeasureNode(1, 3, [0], QNopt.local_RY, 1)],
+        )
+
+        postmap = np.array([[1, 0], [0, 1], [0, 1]])
+
+        mutual_info = QNopt.mutual_info_cost_fn(ansatz, priors, postmap=postmap)
+
+        np.random.seed(12)
+        scenario_settings = ansatz.rand_scenario_settings()
+
+        opt_dict = QNopt.gradient_descent(
+            mutual_info, scenario_settings, step_size=0.1, num_steps=28, sample_width=24
+        )
+
+        assert np.isclose(opt_dict["scores"][-1], match, atol=0.0005)
