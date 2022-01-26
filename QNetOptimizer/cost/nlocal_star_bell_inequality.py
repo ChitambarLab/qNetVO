d22d6ed2e19130c39dd8e7559aa6f7ed779c93e9
Brian Doolittle <brian.d.doolittle@gmail.com>
2022-01-26 08:52:43 -0600

    Update QNetOptimizer/cost/nlocal_star_bell_inequality.py [skip ci]
    Co-authored-by: Tom Bromley <49409390+trbromley@users.noreply.github.com>

--- QNetOptimizer/cost/nlocal_star_bell_inequality.py
+++ QNetOptimizer/cost/nlocal_star_bell_inequality.py
@@ -64,7 +64,7 @@ def star_J22_fn(network_ansatz, **qnode_kwargs):
     :type network_ansatz: QNopt.NetworkAnsatz
 
     :param qnode_kwargs: keyword args passed through to the QNode constructor.
-    :type: *optional* dictionary
+    :type qnode_kwargs: *optional* dictionary
 
     :returns: A function callable as ``J22(scenario_settings)`` that evaluates the :math:`J^n_{22}`
               quantity for the given ``scenario_settings``.
