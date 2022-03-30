fixing gradient bugs

src/qnetvo/gradient_descent.py
@@ -87,6 +87,9 @@ def gradient_descent(
     start_datetime = datetime.utcnow()
     elapsed = 0
 
+    if grad_fn is None:
+        grad_fn = qml.grad(cost, argnum=0)
+
     # performing gradient descent
     for i in range(num_steps):
         if i % sample_width == 0:
@@ -99,16 +102,7 @@ def gradient_descent(
 
         start = time.time()
         if interface == "autograd":
-            # settings = opt.step(cost, settings, grad_fn=grad_fn)
-            settings_update = opt.step(cost, settings, grad_fn=grad_fn)
-            settings = [
-                [
-                    np.array(node_settings, requires_grad=True)
-                    for node_settings in settings_update[i]
-                ]
-                for i in [0, 1]
-            ]
-
+            settings = opt.step(cost, settings, grad_fn=grad_fn)
         elif interface == "tf":
             # opt.minimize updates settings in place
             tf_cost = lambda: cost(settings)

src/qnetvo/network_ansatz.py
@@ -256,7 +256,7 @@ class NetworkAnsatz:
 
         prep_settings = self.layer_settings(scenario_settings[0], prep_inputs, self.prepare_nodes)
         meas_settings = self.layer_settings(scenario_settings[1], meas_inputs, self.measure_nodes)
-        return math.concatenate([prep_settings, meas_settings])
+        return np.concatenate([prep_settings, meas_settings])
 
     @staticmethod
     def circuit_layer(network_nodes):
@@ -294,13 +294,13 @@ class NetworkAnsatz:
         :rtype: list[list[np.array]]
         """
         prepare_settings = [
-            2 * np.pi * np.random.random(node.settings_dims, requires_grad=True) - np.pi
+            2 * np.pi * np.random.random(node.settings_dims) - np.pi
             if len(node.static_settings) == 0
             else np.array([[]])
             for node in self.prepare_nodes
         ]
         measure_settings = [
-            2 * np.pi * np.random.random(node.settings_dims, requires_grad=True) - np.pi
+            2 * np.pi * np.random.random(node.settings_dims) - np.pi
             if len(node.static_settings) == 0
             else np.array([[]])
             for node in self.measure_nodes
@@ -335,15 +335,11 @@ class NetworkAnsatz:
         :rtype: list[list[np.array]]
         """
         prepare_settings = [
-            np.zeros(node.settings_dims, requires_grad=True)
-            if len(node.static_settings) == 0
-            else np.array([[]])
+            np.zeros(node.settings_dims) if len(node.static_settings) == 0 else np.array([[]])
             for node in self.prepare_nodes
         ]
         measure_settings = [
-            np.zeros(node.settings_dims, requires_grad=True)
-            if len(node.static_settings) == 0
-            else np.array([[]])
+            np.zeros(node.settings_dims) if len(node.static_settings) == 0 else np.array([[]])
             for node in self.measure_nodes
         ]
 

test/chsh_gradient_descent_test.py
@@ -48,7 +48,7 @@ class TestCHSHGradientDescent:
         np.random.seed(666)
         init_settings = chsh_ansatz.rand_scenario_settings()
         opt_dict = qnet.gradient_descent(
-            chsh_cost, init_settings, num_steps=30, step_size=0.2, verbose=False
+            chsh_cost, init_settings, num_steps=40, step_size=0.2, verbose=False
         )
 
         assert np.isclose(opt_dict["opt_score"], 2 * np.sqrt(2), atol=1e-3)

test/cost/qnodes_test.py
@@ -75,11 +75,11 @@ class TestQNodes:
         ansatz = qnet.NetworkAnsatz(prep_nodes, meas_nodes)
         qnode = qnet.global_parity_expval_qnode(ansatz)
 
-        assert qnode([0, 0, 0, 0]) == 1
-        assert qnode([0, 1, 0, 0]) == -1
-        assert qnode([0, 0, 1, 1]) == 1
-        assert qnode([1, 0, 1, 1]) == -1
-        assert qnode([1, 1, 1, 1]) == 1
+        assert qnode(np.array([0, 0, 0, 0])) == 1
+        assert qnode(np.array([0, 1, 0, 0])) == -1
+        assert qnode(np.array([0, 0, 1, 1])) == 1
+        assert qnode(np.array([1, 0, 1, 1])) == -1
+        assert qnode(np.array([1, 1, 1, 1])) == 1
 
     def test_joint_probs_qnode(self):
         prep_nodes = [

test/gradient_descent_test.py
@@ -9,7 +9,7 @@ import qnetvo as qnet
 class TestGradientDescent:
     def test_quadratic_cost(self):
         cost = lambda x: x ** 2
-        settings = np.array(2.0, requires_grad=True)
+        settings = np.array(2.0)
         opt_dict = qnet.gradient_descent(cost, settings, num_steps=50, step_size=0.1, verbose=False)
 
         assert np.isclose(opt_dict["opt_score"], 0, atol=1e-6)