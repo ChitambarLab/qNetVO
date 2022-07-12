ae25cb74db9ae82a492e30cd2e072a9251216441
Danny Chen <dannychen@mcswl040.mcs.anl.gov>
2022-07-12 18:06:37 -0400

    added von neumann entropy feature by optimizing measurement basis

--- src/qnetvo/cost/mutual_info.py
+++ src/qnetvo/cost/mutual_info.py
@@ -99,3 +99,45 @@ def mutual_info_cost_fn(
         return -(mutual_info)
 
     return cost_fn
+
+
+def von_neumann_entropy(ansatz, **qnode_kwargs):
+    """Constructs an function that finds the von Neumann entropy through optimization
+
+        The von Neumann entropy quantifies how mixed a quantum state :math:'rho' is.
+        It is defined by the expression:
+        .. math::
+
+                S(\\rho) = Tr(\\rho \\log(\\rho))
+
+        where the :math:'\\log' operator here refers to the matrix logarithm. For any given state,
+        if we measure in its eigenbasis, the Shannon entropy is minimized and coincides with the
+        von Neumann entropy. As the measurement basis deviates from the eigenbases, the measurement
+        outcomes becomes noisier and Shannon entropy diverge from the von Neumann entropy. Thus,
+        the von Neumann entropy will be found by numerically finding the eigenbasis via minimizing
+        the Shannon entropy.
+
+        :param ansatz: The ansatz circuit on which the von Neumann entropy is evalutated.
+        :type ansatz: NetworkAnsatz
+
+        :param qnode_kwargs: Keyword arguments passed to the execute qnodes.
+        :type qnode_kwargs: dictionary
+
+        :returns: A cost function ``von_neumann_entropy(scenario_settings)`` parameterized by
+                  the ansatz-specific scenario settings.
+        :rtype: Function
+        """
+    num_prep_nodes = len(ansatz.prepare_nodes)
+    num_meas_nodes = len(ansatz.measure_nodes)
+
+    probs_qnode = joint_probs_qnode(ansatz, **qnode_kwargs)
+
+    def cost_fn(scenario_settings):
+        settings = ansatz.qnode_settings(scenario_settings, [0] * num_prep_nodes, [0] * num_meas_nodes)
+        py_vec = probs_qnode(settings)
+
+        Hy = shannon_entropy(py_vec)
+
+        return Hy
+
+    return cost_fn
