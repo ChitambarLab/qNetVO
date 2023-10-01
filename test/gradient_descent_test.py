f841cb8ad1659888f7e306450c452acd495338c2
Brian Doolittle <brian.d.doolittle@gmail.com>
2023-10-01 13:54:59 -0400

    adding support for adam optimizer

--- test/gradient_descent_test.py
+++ test/gradient_descent_test.py
@@ -44,3 +44,19 @@ class TestGradientDescent:
                 verbose=False,
                 interface="jax",
             )
+
+        # adam optimizer
+        ad_opt_dict = qnet.gradient_descent(
+            cost,
+            settings,
+            num_steps=50,
+            step_size=0.1,
+            verbose=True,
+            optimizer="adam",
+        )
+
+        assert np.isclose(opt_dict["opt_score"], 0, atol=1e-6)
+        assert np.isclose(opt_dict["opt_settings"], 0, atol=1e-4)
+        assert opt_dict["samples"] == [0, 25, 50]
+        assert len(opt_dict["scores"]) == 3
+        assert len(opt_dict["settings_history"]) == 51
